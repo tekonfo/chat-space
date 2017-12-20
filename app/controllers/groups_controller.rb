1b6166b5c544661388f1aef90b844c58ee89eefa
近藤大翔 <taisho90@icloud.com>
2017-12-21 07:38:32 +0900

    新しくグループを作れるようにした

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,19 @@
 class GroupsController < ApplicationController
 
+  def new
+    @group = Group.new
+  end
+
   def create
     binding.pry
-    User.create(params_permit)
+    group = Group.new
+    group.update(params_permit)
+    redirect_to controller: :messages, action: :index
   end
 
 
     def params_permit
       ##今後メンバーも取得する。
-      params.require(:group).permit(:name)
+      params.require(:chat_group).permit(:name)
     end
 end
