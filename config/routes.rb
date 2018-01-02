da06b7bed8a019627f7eb66e620faf2f95cfbb9a
近藤大翔 <taisho90@icloud.com>
2018-01-02 13:48:19 +0900

    Userの編集画面のルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-
+  resources :users,only: [:edit,:update]
   devise_for :users
 
 ##これはアクションに対するURLを変更してあげているだけだった
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-  get "/users/edit2", :to => "users#edit"
+
   patch "/users/update", :to => "users#update"
   resources :groups,:except => [:index]
 
