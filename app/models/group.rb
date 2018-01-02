6e0b57d557c94de3d520ab86b1405868f64555bd
近藤大翔 <taisho90@icloud.com>
2018-01-02 14:12:07 +0900

    Groupモデル修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,7 @@
 class Group < ApplicationRecord
   validates :name, presence: true
-  has_many :usergroups, dependent: :destroy
+  has_many :usergroups
   has_many :users, through: :usergroups
-  accepts_nested_attributes_for :usergroups, allow_destroy: true,reject_if: :reject_posts
-
-  def reject_posts(attributed)
-    attributed['user_ids'].blank?
-  end
+  accepts_nested_attributes_for :usergroups,allow_destroy: true, reject_if: proc { |attributes| attributes['user_ids'].blank? }
 
 end
