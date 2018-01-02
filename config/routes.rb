22d32ae9a9af4b8d8d2904708a67f92d1d8ad35e
近藤大翔 <taisho90@icloud.com>
2018-01-02 14:10:52 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-
   resources :users,only: [:edit,:update]
   devise_for :users
-
-##これはアクションに対するURLを変更してあげているだけだった
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-
-  patch "/users/update", :to => "users#update"
   resources :groups,:except => [:index]
-
-
 end
