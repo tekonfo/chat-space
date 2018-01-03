8dbef2f866d2efcb7c690482e4da6078950fcb3f
近藤大翔 <taisho90@icloud.com>
2018-01-03 15:25:41 +0900

    Messageテーブルの追加、アソシエーションの追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,8 @@
 class Group < ApplicationRecord
-
   validates :name, presence: true
-
   has_many :usergroups
-
   has_many :users, through: :usergroups
-
+  has_many :messages
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
  has_many :usergroups
  has_many :groups,through: :usergroups
+ has_many :messages
 end
