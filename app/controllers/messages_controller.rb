8860f0bd9844c47191ce9cdaf644d4f6fa2d4a93
近藤大翔 <taisho90@icloud.com>
2018-01-04 19:07:06 +0900

    Imageファイルのアップロード機能の追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,17 @@
 class MessagesController < ApplicationController
   def index
-    flash.now[:notice] = "#{params[:notice]}"
-    @group = Group.new
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @message = Message.new
+    @messages = @group.messages
+  end
+  def create
+    binding.pry
+    message = Message.create(message_params)
+    redirect_to  action: :index
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id].to_i,user_id: current_user.id)
   end
 end
