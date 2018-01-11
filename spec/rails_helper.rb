e0be2f67af4b9fafe2b02d73e5d0b3a460a94eec
近藤大翔 <taisho90@icloud.com>
2018-01-11 13:17:35 +0900

    FactoryGirlの記述の省略

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
+  config.include FactoryGirl::Syntax::Methods
   # instead of true.
   config.use_transactional_fixtures = true
 
