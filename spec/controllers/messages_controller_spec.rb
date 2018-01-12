98f30a8860aed2df6954d41908ed0f758e73c7ac
近藤大翔 <taisho90@icloud.com>
2018-01-12 17:05:28 +0900

    コントローラーのレコードの保存のテスト

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -24,6 +24,29 @@ describe MessagesController do
       end
     end
 
+
+      context 'can not save' do
+        before do
+          login user
+          get :index, params: { group_id: group.id }
+        end
+
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, text: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+        it 'does not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+
     context 'not log in' do
       before do
         get :index, params: { group_id: group.id }
@@ -33,6 +56,8 @@ describe MessagesController do
         expect(response).to redirect_to(new_user_session_path)
       end
     end
+
+
 
 
     end
