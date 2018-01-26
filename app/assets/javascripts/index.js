76b8449e4569a6b5c22842dd7ec75cee857c26ac
近藤大翔 <taisho90@icloud.com>
2018-01-26 18:28:14 +0900

    インデントの修正

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,19 +4,20 @@ $(function(){
 
   function buildHTML(user){
     var html = `
-<div class="chat-group-user clearfix">
-  <p class="chat-group-user__name">${user.name}</p>
-  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-</div>`
+      <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+      </div>`
     return html;
   }
 
     function addHTML(user){
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-  <input name='group[user_ids][]' type='hidden' value="${user.attr("data-user-id")}">
-  <p class='chat-group-user__name'>${user.attr("data-user-name")}</p>
-  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-</div>`
+    var html = `
+      <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value="${user.attr("data-user-id")}">
+      <p class='chat-group-user__name'>${user.attr("data-user-name")}</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
     return html;
   }
 
@@ -55,8 +56,6 @@ $(function(){
 
 
   $(document).on("click",".user-search-add", function() {
-//すでにいるチャットメンバーは消さないといけない
-
     $input = $(this);
     var add_user_html = addHTML($input);
     $("#search-users").append(add_user_html);
